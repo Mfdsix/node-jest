9314b448d4802e6622008b95ab35dd840af6a811
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2022-05-29 10:41:39 +0700

    first commit

---
+++ calculation.js
@@ -0,0 +1,27 @@
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError"
+    }
+}
+
+const averageExams = (valuesExam) => {
+    const numberValidation = valuesExam.every(val => typeof val === "number")
+    if (!numberValidation) throw new ValidationError("All Values Should Be Number")
+    const valueValidation = valuesExam.every(val => val >= 0 && val <= 100)
+    if (!valueValidation) throw new ValidationError("All Values Should Be Between 0 and 100")
+
+    const sumValues = valuesExam.reduce((accumulator, val) => accumulator + val, 0)
+    return (sumValues != 0 && valuesExam.length > 0) ? sumValues / valuesExam.length : 0
+}
+
+const isStudentPassExam = (valuesExam, name) => {
+    const minValues = 75
+    const average = averageExams(valuesExam)
+
+    return average >= minValues
+}
+
+module.exports = {
+    averageExams, isStudentPassExam
+}
